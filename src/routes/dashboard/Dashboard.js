1b958efee5657e3677e656e2ca0fb83fcc5f7f52
Josh D <joj237@gmail.com>
2024-07-10 14:32:45 -0500

    task 8 routing

--- src/routes/dashboard/Dashboard.js
+++ src/routes/dashboard/Dashboard.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../../components/navigation/Header';
 import Sidebar from '../../components/navigation/Sidebar';
+import HomePage from './HomePage'; // This will be created later
+import Favorites from './Favorites'; // This will be created later
+import WatchLater from './WatchLater'; // This will be created later
 import './dashboard.css';
 
 const Dashboard = ({ username, setIsLoggedIn }) => {
 	return (
-		<div className="dashboard">
-			<Header userUsername={username} setIsLoggedIn={setIsLoggedIn} />
-			<Sidebar />
-		</div>
+		<BrowserRouter>
+			<div className="dashboard">
+				<Header userUsername={username} setIsLoggedIn={setIsLoggedIn} />
+				<Sidebar />
+				<div className="dashboard-content">
+					<Routes>
+						<Route path="/home" element={<HomePage />} />
+						<Route path="/favorites" element={<Favorites />} />
+						<Route path="/watchlater" element={<WatchLater />} />
+						<Route path="*" element={<Navigate to="/home" />} />
+					</Routes>
+				</div>
+			</div>
+		</BrowserRouter>
 	);
 };
 
---
+++ src/routes/dashboard/WatchLater.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WatchLater = () => {
+	return <div>Watch Later Page</div>;
+};
+
+export default WatchLater;
